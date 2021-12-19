3648b9f4437369d439c4f58e4e88bb741962153a
MezemovRufetCode <rufatam@code.edu.az>
2021-12-20 01:31:37 +0400

    Add project files.

---
+++ FrontApp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontApp.Controllers
+{
+    public class HomeController:Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ProductList()
+        {
+            return View();
+        }
+        public ActionResult TabsPage()
+        {
+            return View();
+        }
+        public ActionResult AccourdionPage()
+        {
+            return View();
+        }
+        public ActionResult ShopListPage()
+        {
+            return View();
+        }
+        public ActionResult ProgressBarPage()
+        {
+            return View();
+        }
+
+    }
+}
